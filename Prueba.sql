87f5a2f80fd4703dde06c1a89388e64547083eed
JuanCamiloMartinez94 <>
2017-09-29 08:05:45 -0500

    Este commit lo subi desde oracle y no por git

--- Prueba.sql
+++ Prueba.sql
@@ -115,3 +115,7 @@ INSERT INTO Customers VALUES (customer_sequence.nextval, 'Wynne','Alford','Quisq
 INSERT INTO Customers VALUES (customer_sequence.nextval, 'Calvin','Christian','nulla@egettincidunt.edu','P.O. Box 305, 9732 Metus Av.','014-307-3231','28931985');
 
 select ID,EMAIL from customers;
+
+select first_name, last_name from customers;
+
+
